1ade516a653c9f6fa988dd20ffd4c93b57d1237d
aki.suzuki <kaiser@kaiser.ac>
2018-07-04 12:41:37 +0900

    update main

--- base/vstest/main.cpp
+++ base/vstest/main.cpp
@@ -2,6 +2,8 @@
 
 // C ランタイム ヘッダー ファイル
 #include <windows.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
 #include "common.h"
 #include "core.h"
 
@@ -10,9 +12,19 @@
 
 #include <string>
 
-
+void display()
+{}
 _MAIN()
 {
+	nammu::core dr;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutCreateWindow("hello");
+	glutDisplayFunc(display);
+	dr.init();
+	glClearColor(0.0, 1.0, 1.0, 1.0); //背景色
+
+	glutMainLoop();
 	__log("hello\n");
 	return 0;
 }
